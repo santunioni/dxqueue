80153756bd4e0ca46a0d743e9257280c637f9d28
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-07-22 22:16:16 -0300

    chore: disable eslint fail on lazy SqsConsumer and SqsProducer

--- src/initialization/factories.ts
+++ src/initialization/factories.ts
@@ -23,6 +23,7 @@ export function createConsumer<P extends unknown[]>(
   }
 
   if (config.backend.type === 'sqs') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { SqsConsumer } = require('../backends/sqs')
     return new SqsConsumer(func, messageConfig, config.backend)
   }
@@ -45,6 +46,7 @@ export function createProducer<P extends unknown[]>(
   }
 
   if (config.backend.type === 'sqs') {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { SqsProducer } = require('../backends/sqs')
     return new SqsProducer(messageConfig, config.backend)
   }
