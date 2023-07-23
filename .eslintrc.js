90193615d677ab9a436c507941e63a9224e6a90e
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-07-22 21:34:15 -0300

    chore: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,11 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {},
+    "ignorePatterns": ["dist/**/*.js", ".eslintrc.js"],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "plugins": [
+        "@typescript-eslint"
+    ]
 }
