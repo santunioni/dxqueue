7455aa31b177436b9138763f877a8b196fbf5a80
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-07-23 07:50:55 -0300

    Revert "chore: remove unknown cast"
    This reverts commit db08ad9151049a0b5914931a812412fac085eb61.

--- src/decorator.ts
+++ src/decorator.ts
@@ -92,7 +92,7 @@ export function Queue<
 
     return {
       get() {
-        const config = cachedConfigFactory(this as DecoratedClass)
+        const config = cachedConfigFactory(this as unknown as DecoratedClass)
         const producer = createProducer(descriptor.value.bind(this), config)
         const publisher = (...args: any) => producer.publish(...args)
 
