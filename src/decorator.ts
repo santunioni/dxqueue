763c127a9bc77f5e492a70e33f9d39db96f57304
Vinícius Vargas <vinicius.vargas@loft.com.br>
2023-07-22 21:51:15 -0300

    chore: rename generics

--- src/decorator.ts
+++ src/decorator.ts
@@ -22,7 +22,9 @@ class DefaultWeakMap<K extends object, V> extends WeakMap<K, V> {
  * Wrap a single-argument function in a memoization layer.
  * @param func
  */
-function memoizeeSingleArgument<I, R>(func: (i: I) => R) {
+function memoizeeSingleArgument<I, R>(
+  func: (instance: I) => R,
+): (intance: I) => R {
   const cache = new Map<I, R>()
   return function (i: I) {
     let result = cache.get(i)
@@ -38,10 +40,17 @@ type ConfigFactory<T extends object, P extends unknown[]> = (
   instance: T,
 ) => Config<P>
 
+type DecoratedClassMethod<
+  DecoratedClass extends object,
+  DecoratedMethodKey extends keyof DecoratedClass & string,
+> = DecoratedClass[DecoratedMethodKey] extends Fn<any[]>
+  ? DecoratedClass[DecoratedMethodKey]
+  : never
+
 type QueueDecoratorParams<
-  T extends object,
-  K extends keyof T & string,
-> = Parameters<T[K] extends Fn<any[]> ? T[K] : never>
+  DecoratedClass extends object,
+  DecoratedMethodKey extends keyof DecoratedClass & string,
+> = Parameters<DecoratedClassMethod<DecoratedClass, DecoratedMethodKey>>
 
 const ORIGINAL_CLASS_DESCRIPTORS = new DefaultWeakMap<
   object,
@@ -60,10 +69,20 @@ const ORIGINAL_CLASS_DESCRIPTORS = new DefaultWeakMap<
  * @param configFactory
  */
 export function Queue<
-  T extends object,
-  K extends keyof T & string = keyof T & string,
->(configFactory: ConfigFactory<T, QueueDecoratorParams<T, K>>) {
-  return function (clsPrototype: T, methodName: K, descriptor) {
+  DecoratedClass extends object,
+  DecoratedMethodKey extends keyof DecoratedClass &
+    string = keyof DecoratedClass & string,
+>(
+  configFactory: ConfigFactory<
+    DecoratedClass,
+    QueueDecoratorParams<DecoratedClass, DecoratedMethodKey>
+  >,
+) {
+  return function (
+    clsPrototype: DecoratedClass,
+    methodName: DecoratedMethodKey,
+    descriptor,
+  ) {
     const cachedConfigFactory = memoizeeSingleArgument(configFactory)
 
     ORIGINAL_CLASS_DESCRIPTORS.get(clsPrototype).set(methodName, {
@@ -73,7 +92,7 @@ export function Queue<
 
     return {
       get() {
-        const config = cachedConfigFactory(this as unknown as T)
+        const config = cachedConfigFactory(this as unknown as DecoratedClass)
         const producer = createProducer(descriptor.value.bind(this), config)
         const publisher = (...args: any) => producer.publish(...args)
 
@@ -101,9 +120,10 @@ const METHOD_CONSUMERS_BY_INSTANCE = new DefaultWeakMap<
  * @param method
  */
 export function getConsumerFromInstanceMethod<
-  T extends object,
-  M extends keyof T & string = keyof T & string,
->(instance: T, method: M): Consumer {
+  ConsumerInstanceType extends object,
+  ConsumerInstanceMethod extends keyof ConsumerInstanceType &
+    string = keyof ConsumerInstanceType & string,
+>(instance: ConsumerInstanceType, method: ConsumerInstanceMethod): Consumer {
   let consumer = METHOD_CONSUMERS_BY_INSTANCE.get(instance).get(method)
 
   if (!consumer) {
